e7adfe3946bbf47dd3a7d2ef142ef051bbe1916e
Anvar Akhmadjanov <www.latvienhoasadrizeges@gmail.com>
2022-06-20 13:59:49 +0500

    added router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,14 @@ import {ButtonComponent} from './components/button/button.component';
 import {TasksComponent} from './components/tasks/tasks.component';
 import {TaskItemComponent} from './components/task-item/task-item.component';
 import {AddTaskComponent} from './components/add-task/add-task.component';
+import {RouterModule, Routes} from "@angular/router";
+import {AboutComponent} from './components/about/about.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+const appRoutes: Routes = [
+  {path: '', component: TasksComponent},
+  {path: 'about', component: AboutComponent}
+]
 
 @NgModule({
   declarations: [
@@ -19,12 +27,15 @@ import {AddTaskComponent} from './components/add-task/add-task.component';
     TasksComponent,
     TaskItemComponent,
     AddTaskComponent,
+    AboutComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
     FontAwesomeModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes, {enableTracing: true}),
   ],
   providers: [],
   bootstrap: [AppComponent]
