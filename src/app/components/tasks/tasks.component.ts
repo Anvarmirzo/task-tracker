d407bdedd78fec14ef30fd40e78b8dfbbf8eb72b
Anvarmirzo <64534079+Anvarmirzo@users.noreply.github.com>
2022-06-20 13:43:24 +0500

    added ui service (#9)

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,5 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {TASKS} from '../../mock-tasks';
 import {Task} from '../../Task';
 import {TaskService} from '../../services/task.service';
 
