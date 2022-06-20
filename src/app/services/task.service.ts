0e578d2115bdd5a542f4809f0ede31476bc1adb6
Anvarmirzo <64534079+Anvarmirzo@users.noreply.github.com>
2022-06-20 16:07:04 +0500

    fixed json-server port (#12)

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -12,7 +12,7 @@ const httpOptions = {
   providedIn: 'root'
 })
 export class TaskService {
-  private apiUrl = 'http://localhost:5000/tasks';
+  private apiUrl = `${window.location.protocol}//${window.location.hostname}:13000/tasks`;
 
   constructor(private http: HttpClient) {
   }
