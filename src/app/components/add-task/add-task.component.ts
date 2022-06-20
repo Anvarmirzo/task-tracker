0218ae88217da9facef4a1cb8758df653985c3cd
Anvar Akhmadjanov <www.latvienhoasadrizeges@gmail.com>
2022-06-20 13:05:03 +0500

    added post request

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Task} from "../../Task";
 
 @Component({
   selector: 'app-add-task',
@@ -6,6 +7,7 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./add-task.component.css']
 })
 export class AddTaskComponent implements OnInit {
+  @Output() onAddTask = new EventEmitter<Task>()
   text = '';
   day = '';
   reminder = false;
@@ -16,4 +18,19 @@ export class AddTaskComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onSubmit() {
+    if (!this.text) return alert('Please enter a task');
+
+    const newTask: Task = {
+      text: this.text,
+      day: this.day,
+      reminder: this.reminder
+    }
+
+    this.onAddTask.emit(newTask);
+
+    this.text = '';
+    this.day = '';
+    this.reminder = false;
+  }
 }
